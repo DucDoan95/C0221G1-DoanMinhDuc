32b414ee47dcc49a80eedbd10ef211af6be62496
Minh Đức Đoàn <you@example.com>
2021-03-04 17:19:30 +0700

    update module 7

---
+++ module1/bai7_bien_kieu-du-lieu_toan-tu/thuc_hanh/toan_tu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the Width: ");
+inputHeight = prompt("Enter the Height: ");
+let witdh = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = witdh * height;
+document.write("The area is : " +area);
