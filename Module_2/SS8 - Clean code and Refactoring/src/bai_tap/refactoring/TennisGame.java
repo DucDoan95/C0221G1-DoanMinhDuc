73f9ca7ed24c9e195852f86a8a9ca6af0d1f04f0
Minh Đức Đoàn <you@example.com>
2021-04-11 12:37:20 +0700

    Update

--- Module_2/SS8 - Clean code and Refactoring/src/bai_tap/refactoring/TennisGame.java
+++ Module_2/SS8 - Clean code and Refactoring/src/bai_tap/refactoring/TennisGame.java
@@ -2,12 +2,12 @@ package bai_tap.refactoring;
 
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1Score==player2Score)
         {
-            switch (m_score1)
+            switch (player1Score)
             {
                 case 0:
                     score = "Love-All";
@@ -27,9 +27,9 @@ public class TennisGame {
 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1Score>=4 || player2Score>=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = player1Score-player2Score;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -39,8 +39,8 @@ public class TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1Score;
+                else { score+="-"; tempScore = player2Score;}
                 switch(tempScore)
                 {
                     case 0:
