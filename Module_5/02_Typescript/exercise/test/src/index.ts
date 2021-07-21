a208df739b8b16605b5ef5369b1b3c7ef4713b6d
Minh Đức Đoàn <you@example.com>
2021-07-21 19:21:39 +0700

    Update

--- Module_5/02_Typescript/exercise/test/src/index.ts
+++ Module_5/02_Typescript/exercise/test/src/index.ts
@@ -1,19 +1,43 @@
+// console.log("Start");
+// let promiseObj = new Promise((resolve, rejects) => {
+//     let xHttp = new XMLHttpRequest()
+//     xHttp.onload = function () {
+//         if (this.status == 200) {
+//             resolve(xHttp.responseText);
+//         } else {
+//             rejects("Error call API")
+//         }
+//     };
+//     xHttp.open("GET", "https://api.github.com/search/repositories?q=angular")
+//     xHttp.send();
+// });
+// promiseObj.then((result) => {
+//     console.log("OK")
+//     console.log((result));
+// }, (error) => {
+//     console.log("Pending")
+// }).finally(() => console.log("End"));
+
 console.log("Start");
-let promiseObj = new Promise((resolve, rejects) => {
-    let xHttp = new XMLHttpRequest()
+async function test() {
+    let rs;
+    let xHttp = new XMLHttpRequest();
     xHttp.onload = function () {
         if (this.status == 200) {
-            resolve(xHttp.responseText);
+            rs = (xHttp.responseText);
         } else {
-            rejects("Error call API")
+            rs= ("Error call API");
         }
     };
     xHttp.open("GET", "https://api.github.com/search/repositories?q=angular")
     xHttp.send();
-});
-promiseObj.then((result) => {
+    rs = await xHttp;
+    return rs;
+}
+test().then((result) => {
     console.log("OK")
-    console.log((result));
+    console.log(result)
 }, (error) => {
     console.log("Pending")
 }).finally(() => console.log("End"));
+
